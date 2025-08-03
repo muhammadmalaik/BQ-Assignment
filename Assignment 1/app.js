2542485da6c5eed5ccc112e3a74c71e5724c17bd
Muhammad-Malaik <muhammadmalaik76@gmail.com>
2025-08-03 17:30:29 +0500

    Add files via upload

---
+++ Assignment 1/app.js
@@ -0,0 +1,127 @@
+// 1:
+// let num = 7;
+
+// if (num % 2 === 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// 2:
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not eligible");
+// }
+
+// 3:
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 4:
+// let i = 2;
+// while (i <= 20) {
+//   console.log(i);
+//   i += 2;
+// }
+
+// 5:
+// let str = "hello";
+// let reversed = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reversed += str[i];
+// }
+
+// console.log(reversed); // olleh
+
+// 6:
+// function add(a, b) {
+//   return a + b;
+// }
+
+// 7:
+// function factorial(n) {
+//   let result = 1;
+//   for (let i = 1; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
+
+// 8:
+// function isPalindrome(str) {
+//   let reversed = str.split("").reverse().join("");
+//   return str === reversed;
+// }
+
+// 9:
+// let student = {
+//   name: "Ali",
+//   rollNo: 101,
+//   marks: 85,
+//   isPassed: true
+// };
+
+// 10:
+// console.log(student.name);        
+// console.log(student["marks"]);    
+
+// 11:
+// student.sayHello = function() {
+//   console.log("Hello, I am " + this.name);
+// };
+
+// 12:
+// function printObject(obj) {
+//   for (let key in obj) {
+//     console.log(key + ": " + obj[key]);
+//   }
+// }
+
+// 13:
+// let fruits = ["apple", "banana", "mango"];
+
+// 14:
+// let fruits = ["apple", "banana", "mango"];
+// fruits.push("grapes");
+// console.log(fruits);
+
+// 15:
+// let fruits = ["apple", "banana", "mango"];
+// fruits.pop();
+
+// 16:
+// let fruits = ["apple", "banana", "mango"];
+// console.log(fruits.includes("banana")); // true/false
+// console.log(fruits);
+
+// 17:
+// let nums = [1, 2, 3, 4];
+// let doubled = nums.map(n => n * 2);
+
+// 18:
+// let ages = [15, 22, 18, 14];
+// let adults = ages.filter(age => age >= 18);
+
+// 19:
+// let numbers = [5, 9, 12, 7];
+// let result = numbers.find(n => n > 10);
+
+// 20:
+// let names = ["Ali", "Zara", "Umar"];
+// names.forEach(name => console.log(name));
+
+// 21:
+// const students = [
+//   { name: "Ali", marks: 80 },
+//   { name: "Zara", marks: 95 },
+//   { name: "Umar", marks: 45 }
+// ];
+
+// let namesOnly = students.map(s => s.name);
+
+// 22:
